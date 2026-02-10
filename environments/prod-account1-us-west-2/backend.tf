2b8e479f4ec80ddff2d1d92dccb9c3393e0f8a24
Khurram Saleem Butt <khurramsaleembutt@yahoo.com>
2026-02-10 18:02:40 +0000

    Fix S3 backend region for prod-account1-us-west-2
    - Change backend region from us-west-2 to us-east-1
    - S3 bucket terraform-state-vpc-093285711854 is located in us-east-1
    - Fixes 'requested bucket from us-west-2, actual location us-east-1' error

--- environments/prod-account1-us-west-2/backend.tf
+++ environments/prod-account1-us-west-2/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-vpc-093285711854"
     key            = "vpc/prod-account1-us-west-2/terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks-vpc"
     encrypt        = true
   }
